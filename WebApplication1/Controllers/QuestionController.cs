b7137f8e084c4930d434602910c19914556935d6
Zuhair Saleem <117568070+zuhair0@users.noreply.github.com>
2023-06-11 20:36:00 -0700

    api added

---
+++ WebApplication1/Controllers/QuestionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IntelligenceTestModel;
+using IntelligenceTestDAL;
+namespace IntelligenceTest.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class QuestionController : Controller
+    {
+        [HttpGet]
+        [Route("GetQuestion")]
+        public async Task<JsonResult> GetQuestions()
+        {
+            List<QuestionsModel> questions = new List<QuestionsModel>();
+            questions = QuestionsDAL.GetAllQuestions();
+            if (questions.Count > 0)
+            {
+                return new JsonResult(questions);
+            }
+            else
+            {
+                return new JsonResult("Not Found");
+            }
+        }
+    }
+}
